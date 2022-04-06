dbfb75b874a629e6dc530f7ec0b65ea592a9f655
GGLaDOS21 <emanuele.bongiovanni42@gmail.com>
2022-04-06 18:21:55 +0200

    Completo

--- Lab3/main.py
+++ Lab3/main.py
@@ -2,6 +2,7 @@ import json
 import math
 import matplotlib
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class Signal_information:
     def __init__(self, signal_power, path):
@@ -11,6 +12,12 @@ class Signal_information:
         self.path = []
         for p in path:
             self.path.append(p)
+    def getLatency(self):
+        return self.latency
+    def getNoise(self):
+        return self.noise_power
+    def getPower(self):
+        return self.signal_power
 
     def sigPowUpdate(self, increment):
         self.signal_power += increment
@@ -25,6 +32,8 @@ class Signal_information:
         return self.path.pop(0)
 
     def nextHop(self):
+        if(len(self.path) == 0):
+            return None
         return self.path[0]
 
 class Node:
@@ -35,13 +44,16 @@ class Node:
         self.successive = {}
 
     def propagate(self, signal):
-        if signal.pathUpdate == self.label:
-            nextNode = signal.nextHop
+        if signal.pathUpdate() == self.label:
+            nextNode = signal.nextHop()
+            if(nextNode == None):
+                return
             for nodes in self.connected_nodes:
                 if nodes == nextNode:
-                    self.successive[nextNode].propagate
+                    self.successive[nextNode].propagate(signal)
+                    break
         else:
-            return signal
+            return
 
 class Line:
     def __init__(self, label, length):
@@ -50,16 +62,16 @@ class Line:
         self.successive = {}
 
     def latency_generation(self, signal):
-        signal.latencyUpdate(signal, self.length/200000)
+        signal.latencyUpdate(self.length/200000)
 
     def noise_generation(self, signal):
-        signal.noisePowUpdate(signal.signal_power * self.length * 10^(-9))
+        signal.noisePowUpdate(signal.signal_power * self.length * math.pow(10, -9))
 
     def propagate(self, signal):
         self.noise_generation(signal)
         self.latency_generation(signal)
         node = signal.nextHop()
-        self.successive[node].propagate
+        self.successive[node].propagate(signal)
 
 
 class Network:
@@ -76,6 +88,10 @@ class Network:
 
         for node in self.nodes:
             for conn in self.nodes[node].connected_nodes:
+                if((node + conn) in self.lines):
+                    continue
+                if((conn + node) in self.lines):
+                    continue
                 label = node + conn
                 node1 = self.nodes[node]
                 node2 = self.nodes[conn]
@@ -83,14 +99,22 @@ class Network:
                 ln = Line(label, dist)
                 self.lines.update({label: ln})
 
+    def get_nodes(self):
+        return self.nodes
+
     def connect(self):
         for key in self.nodes:
             node = self.nodes[key]
             for conn in node.connected_nodes:
                 lbl = key + conn
-                line = self.lines[lbl]
+                if(lbl in self.lines):
+                    line = self.lines[lbl]
+                else:
+                    lbl = conn + key
+                    line = self.lines[lbl]
                 node.successive.update({conn: line})
                 line.successive.update({conn: self.nodes[conn]})
+                line.successive.update({key: node})
 
     def find_path(self, nA, nB):
         nodeA = self.nodes[nA]
@@ -134,20 +158,31 @@ class Network:
 
 
 
-
-
-
-
-
-
 net = Network("nodes.json")
+net.connect()
+#net.draw()
+#plt.show()
+dict_list = {}
+nodes = net.get_nodes()
+table_data = {}
+for key1 in nodes:
+    for key2 in nodes:
+        if (key1 != key2):
+            if(key1 + key2) in dict_list:
+                continue
+            if(key2 + key1) in dict_list:
+                continue
+            dict_list.update({(key1 + key2): net.find_path(key1, key2)})
+for key in dict_list:
+    path_list = dict_list[key]
+    for path in path_list:
+        signal = Signal_information(1*math.pow(10,-3),path)
+        net.propagate(signal)
+        signal_data={"Latency": signal.getLatency(),"Noise": signal.getNoise(),"Signal/Noise(dB)": 10 * math.log10(signal.getPower()/signal.getNoise())}
+        table_data.update({key: signal_data})
+dataframe = pd.DataFrame(table_data)
 
-net.draw()
-plt.show()
-
-
-
-
+print(dataframe)
 
 
 
